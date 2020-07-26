7a622281e755a250563dab548fa47cd73339d4c4
SergeiKorshunov <58481594+SergeiKorshunov@users.noreply.github.com>
2020-07-26 22:29:32 +0300

    local-v1

--- dest/js/script.js
+++ dest/js/script.js
@@ -1,82 +1,97 @@
-// modal
+document.addEventListener('DOMContentLoaded', function(e){
+    // e.preventDefault();
+    // modal
+    const btn = document.querySelector('.header-log-js');
+    const modal = document.querySelector('.overlay-js');
+    const agree = document.querySelector('.agree-js');
 
-;function modalShow(trigger,modal,agree){
-    trigger.addEventListener('click', function(e){
-        if(e.target){
-            e.preventDefault();
-        }
-        modal.style.display = 'flex';
-        document.body.style.overflow = 'hidden';
-        
-        agree.addEventListener('click', function(){
-            modal.style.display = 'none';
-            document.body.style.overflow = 'visible';
-        })
+    const auth = document.querySelector('.agree-js');
+    const log = document.querySelector('#log-js').value;
+    const password = document.querySelector('#pass-js').value;
+    const user = document.querySelector('.persona');
+    const checked = document.querySelector('check-js');
 
-        modal.addEventListener('click',function(e){
-            if(e.target === modal){
-                modal.style.display ='none'
-                document.body.style.overflow = 'visible';
-            }
-        })
-    })
-}
-const btn = document.querySelector('.header-log-js');
-const modal = document.querySelector('.overlay-js');
-const agree = document.querySelector('.agree-js');
-modalShow(btn, modal, agree);
-
-
-// tabs
-
-;document.querySelectorAll('[data-category]').forEach(item => {
-    item.addEventListener('click', function(e){
-        e.preventDefault();
-        const tabId = this.dataset.category;
-        // console.log(id);
-        document.querySelectorAll('.menu-link-js').forEach(item=>{
-            item.classList.remove('active-link-js');
-            this.classList.add('active-link-js');
-        })
-
-        document.querySelectorAll('.tab-cat').forEach(item =>{
-            item.classList.add('hide-tab-block');
-        })
-        document.getElementById(tabId).classList.toggle('hide-tab-block');
-    })
     
-});
+    
+    const person = {
+        login: log.value,
+        pass: password.value
+    }
 
-// scrollBar
+    user.textContent = localStorage.getItem(person.login);
+    // user.innerHTML = localStorage.getItem(person.login);
 
-new SimpleBar(document.querySelector('.scrollTv'), {
-    autoHide: false,
-    scrollbarMinSize: 350,
-});
+    ;function modalShow(trigger, modal, agree) {
+        trigger.addEventListener('click', function (e) {
+            if (e.target) {
+                e.preventDefault();
+            }
+            modal.style.display = 'flex';
+            document.body.style.overflow = 'hidden';
 
-// form
+            agree.addEventListener('click', function () {
+                modal.style.display = 'none';
+                document.body.style.overflow = 'visible';
+            })
 
-const auth = document.querySelector('.agree-js');
-const log = document.querySelector('#log-js');
-const password = document.querySelector('#pass-js');
-const user = document.querySelector('.persona');
-const checked = document.querySelector('check-js');
+            modal.addEventListener('click', function (e) {
+                if (e.target === modal) {
+                    modal.style.display = 'none'
+                    document.body.style.overflow = 'visible';
+                }
+            })
+        })
+    }
 
-auth.addEventListener('click', function(){
-    const a = log.value;
-    const b = password.value;
-    // if(checked === true){
-    // }
-    const arr = [];
-    arr.push(a,b)
-    localStorage.setItem('log' , arr);
-    console.log(checked);
-    user.textContent = a + ".";
+    modalShow(btn, modal, agree);
+
+    // tabs
+
+    ; document.querySelectorAll('[data-category]').forEach(item => {
+        item.addEventListener('click', function (e) {
+            e.preventDefault();
+            const tabId = this.dataset.category;
+            // console.log(id);
+            document.querySelectorAll('.menu-link-js').forEach(item => {
+                item.classList.remove('active-link-js');
+                this.classList.add('active-link-js');
+            })
+
+            document.querySelectorAll('.tab-cat').forEach(item => {
+                item.classList.add('hide-tab-block');
+            })
+            document.getElementById(tabId).classList.toggle('hide-tab-block');
+        })
+
+    });
+
+    // scrollBar
+
+    new SimpleBar(document.querySelector('.scrollTv'), {
+        autoHide: false,
+        scrollbarMinSize: 350,
+    });
+
+    // form
+
+
+    auth.addEventListener('click', function () {
+        
+        // log[0].toUpperCase() + log.substring(1);
+        
+        // localStorage.setItem('person', JSON.stringify('person', person.login));
+        localStorage.setItem('person', person);
+
+    })
+
+    // document.addEventListener('storage', function () {
+         // localStorage.getItem(a);
+         // user.textContent = a + " " + b[0].toUpperCase() + ".";
+         // console.log('2');
+         // let out = document.querySelector('.out').textContent = localStorage.getItem('a');
+    // })
+
+    
+
 })
 
-// window.addEventListener('storage', function () {
-//     localStorage.getItem(a);
-//     user.textContent = a + " " + b[0].toUpperCase() + ".";
-//     console.log('2');
-//     let out = document.querySelector('.out').textContent = localStorage.getItem('a');
-// })
